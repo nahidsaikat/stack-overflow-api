4ef089804d4ba4b019753d1e8ec57994034f300d
nahidsaikat <nahidsaikatft40@gmail.com>
2020-07-19 00:12:41 +0600

    Add index view

--- app/views.py
+++ app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+import requests
 
-# Create your views here.
+from django.shortcuts import render
+from django.http.response import JsonResponse
+
+
+def get_questions(request):
+    url = 'https://api.stackexchange.com/2.2/questions?page=2&pagesize=25&fromdate=1593561600&todate=1595030400&order=desc&min=1593561600&max=1595030400&sort=activity&tagged=python;c&site=stackoverflow'
+    question_list = requests.get(url)
+    return question_list.json()
+
+
+def index(request, *args, **kwargs):
+    return JsonResponse(get_questions(request))
+
