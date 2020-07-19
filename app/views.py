069cb2edb35faf8975f22a0d792bb7a8d433effc
nahidsaikat <nahidsaikatft40@gmail.com>
2020-07-19 16:09:24 +0600

    Add questions table in index page

--- app/views.py
+++ app/views.py
@@ -1,15 +1,21 @@
 import requests
 
 from django.shortcuts import render
-from django.http.response import JsonResponse
 
 
 def get_questions(request):
-    url = 'https://api.stackexchange.com/2.2/questions?page=1&pagesize=25&fromdate=1593561600&todate=1595030400&order=desc&min=1593561600&max=1595030400&sort=activity&tagged=python;c&site=stackoverflow'
-    question_list = requests.get(url)
-    return question_list.json()
+    page = request.GET.get('page', 1)
+    url = f'https://api.stackexchange.com/2.2/questions?page={page}&pagesize=25&fromdate=1593561600&todate=1595030400&order=desc&min=1593561600&max=1595030400&sort=activity&tagged=python;c&site=stackoverflow'
+    questions = requests.get(url)
+    return questions.json()
 
 
 def index(request, *args, **kwargs):
-    return render(request, 'index.html', context={})
+    page = int(request.GET.get('page', 1))
+    questions = get_questions(request)
+    return render(request, 'index.html', {
+        'questions': questions,
+        'pre_page': page-1,
+        'next_page': page+1,
+    })
 
